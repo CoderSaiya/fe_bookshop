e8dfae98c4457c72cdb254b5b83c17d6d36be129
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2025-08-12 16:10:26 +0700

    register store provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import { CartProvider } from "@/hooks/use-cart";
 import { ThemeProvider } from "@/components/theme-provider";
 import { AuthProvider } from "@/components/auth-provider";
+import {Provider} from "react-redux";
+import {store} from "@/store";
 
 export const metadata: Metadata = {
     title: "The Modern Bookstore",
@@ -20,6 +22,7 @@ export default async function RootLayout({
     return (
         <html lang={locale} suppressHydrationWarning>
             <body className={`antialiased`}>
+            <Provider store={store}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
@@ -30,6 +33,7 @@ export default async function RootLayout({
                         <CartProvider>{children}</CartProvider>
                     </AuthProvider>
                 </ThemeProvider>
+            </Provider>
             </body>
         </html>
     );
